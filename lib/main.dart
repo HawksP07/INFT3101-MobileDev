1abf788d7b98028c7c797c009f5cad0a8befa60d
Amy Ahn <amy.ahn.kr@gmail.com>
2024-12-03 10:18:04 -0500

    main fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
-import './utils/responsive.dart';
+import 'pages/flashcard.dart';
+import 'pages/login.dart';
+import 'pages/settings.dart';
+import 'pages/signup.dart';
 import './widgets/custom_app_bar.dart';
+import './utils/responsive.dart';
 import './widgets/main_button.dart';
+import 'pages/multiplechoice.dart';
+import 'pages/shortanswer.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 255, 255),
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '2AIR'), // Set initial route
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/settings': (context) => const SettingsPage(),
+        '/signup': (context) => const SignupPage(),
+        '/flashcard': (context) => const FlashCardPage(),
+        '/multiplechoice': (context) => const MultipleChoicePage(),
+        '/shortanswer': (context) => const ShortAnswerPage(),
+      },
+    );
+  }
+}
+
+// Main Home Page
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final bool isLoggedIn = true; // Mock login status
+  final bool isLoggedIn = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-        title: '3AIR', // Dynamic title passed to CustomAppBar
+        title: widget.title,
         isLoggedIn: isLoggedIn,
       ),
       body: Stack(
         children: [
-          // Background Image
           Container(
-            alignment: Alignment.center,
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/bg-dark.jpg'),
@@ -34,24 +71,20 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: [
                 SizedBox(height: Responsive.heightPercentage(context, 15)),
-                // Title
                 Text(
                   'KOREAN QUIZ',
-                  style: Theme.of(context).textTheme.displayLarge, // theme
+                  style: Theme.of(context).textTheme.displayLarge,
                 ),
                 SizedBox(height: Responsive.heightPercentage(context, 2)),
-                // Subtitle
                 SizedBox(
                   width: Responsive.widthPercentage(context, 90),
                   child: Text(
                     'Dive Into Building Your Korean with interactive quizzes',
                     textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyLarge, // theme
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
                 ),
                 SizedBox(height: Responsive.heightPercentage(context, 3)),
-
-                // Vocabulary Card Button
                 MainButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/flashcard');
@@ -59,10 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   text: 'Vocabulary Card',
                   icon: Icons.style_outlined,
                 ),
-
                 SizedBox(height: Responsive.heightPercentage(context, 3)),
-
-                // Multiple Choice Button
                 MainButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/multiplechoice');
@@ -70,10 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   text: 'Multiple Choice',
                   icon: Icons.library_add_check_outlined,
                 ),
-
                 SizedBox(height: Responsive.heightPercentage(context, 3)),
-
-                // Short Answer Button
                 MainButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/shortanswer');
@@ -81,13 +108,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   text: 'Short Answer',
                   icon: Icons.textsms_outlined,
                 ),
-
                 const Spacer(),
               ],
             ),
           ),
-
-          // Footer Section
           Positioned(
             bottom: Responsive.heightPercentage(context, 5),
             left: 0,
@@ -97,12 +121,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text(
                   'INFT 3101 Mobile Development',
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge, // theme
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
                 Text(
-                  '\u00a9 2024 3AIR',
+                  '\u00a9 2024 2AIR',
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge, // theme
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
               ],
             ),
